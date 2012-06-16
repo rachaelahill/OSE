95cf2ac38ed5a6f7f8c3feadd121e4091e63c6bb
Rachael Hill <rachaelhill@Rachaels-MacBook-Pro.local>
2012-06-16 10:12:16 -0400

    updating email and adding gvcs listings

--- incs/emailForm.php
+++ incs/emailForm.php
@@ -1,42 +1,10 @@
-<?
-  if(isset($_POST['name']) == '' || isset($_POST['email']) == '' || isset($_POST['mess']) == '')
-  {
-    died('We are sorry, but there appears to be a problem with the form you submitted.');
-  }
-  
-  $emailTo = $_POST['emailTo'];
+<?php
+  $emailTo = 'rachaelann.hill@gmail.com';
+  $subject = 'Email from application.';
   $name = $_POST['name'];
   $email = $_POST['email'];
   $mess = $_POST['mess'];
-  $error_mess = '';
-
-  $email_exp = '/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$';
-  if(!preg_match($email_exp, $email))
-  {
-    $error_mess .= 'The email address you entered does not appear to be valid. <br />';
-  }
+  $header = "Reply to: $email";
   
-  $name_ex = '/^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*$';
-  if(!preg_match($name_ex, $name))
-  {
-    $error_mess .= 'The name you entered does not appear to be valid.';
-  }
-  
-  if(strlen($mess) < 500 || strlen($mess) == 0)
-  {
-    $error_mess .= 'The message you entered does not appear to be valid.';
-  }
-  
-  function clean_string($string)
-  {
-    $bad = array('content-stype', 'bcc:', 'to:', 'cc:', 'href');
-    return str_replace($bad, '', $string);
-  }
-  
-  $emailDetail .= 'Name:'.clean_string($name).'\n';
-  $emailDetail .= 'Email:'.clean_string($email).'\n';
-  $emailDetail .= 'Message:'.clean_string($mess).'\n';
-  
-  @mail($emailTo, $emailDetail);
-
+   mail($emailTo, $subject, $mess, $header) or die("Error");
 ?>
--- incs/main.js
+++ incs/main.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
-  function mongoGetDocs(){
-    //var q = {"csvDays":"1"}
+  var gvcsDetailID;
+  
+//Returns gvcsList(Agri) from MongoHQ
+  function mongoGetGVCSList(){
     $.ajax({
-      url: 'https://api.mongohq.com/databases/oseDB/collections/gvcsDetails/documents',
+      url: 'https://api.mongohq.com/databases/oseDB/collections/gvcsList/documents',
       type: 'GET',
       dataType: 'json',
       data: {
-        "_apikey":"xoxqi26x0rwv9loxhdfa",
-        
+        "_apikey":"xoxqi26x0rwv9loxhdfa"
       },
       success: function(r) {
         console.log("Mongo: success",r[1].title);
-        
+        createGVCSList(r);
       },
       error: function(data) {
         console.log("Mongo: fail",data);
@@ -20,19 +21,195 @@ $(document).ready(function() {
      });
   }
   
+//Returns gvcsEne from MongoHQ
+  function mongoGetGVCSEne(){
+    $.ajax({
+      url: 'https://api.mongohq.com/databases/oseDB/collections/gvcsEne/documents',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        "_apikey":"xoxqi26x0rwv9loxhdfa"
+      },
+      success: function(r) {
+        console.log("Mongo: success",r[1].title);
+        createGVCSList(r);
+      },
+      error: function(data) {
+        console.log("Mongo: fail",data);
+        return false;
+      }
+     });
+  }
+  
+//Returns gvcsInd from MongoHQ
+  function mongoGetGVCSInd(){
+    $.ajax({
+      url: 'https://api.mongohq.com/databases/oseDB/collections/gvcsInd/documents',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        "_apikey":"xoxqi26x0rwv9loxhdfa"
+      },
+      success: function(r) {
+        console.log("Mongo: success",r[1].title);
+        createGVCSList(r);
+      },
+      error: function(data) {
+        console.log("Mongo: fail",data);
+        return false;
+      }
+     });
+  }
+  
+//Returns gvcsMat from MongoHQ
+  function mongoGetGVCSMat(){
+    $.ajax({
+      url: 'https://api.mongohq.com/databases/oseDB/collections/gvcsMat/documents',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        "_apikey":"xoxqi26x0rwv9loxhdfa"
+      },
+      success: function(r) {
+        console.log("Mongo: success",r[1].title);
+        createGVCSList(r);
+      },
+      error: function(data) {
+        console.log("Mongo: fail",data);
+        return false;
+      }
+     });
+  }
+  
+//Returns gvcsTran from MongoHQ
+  function mongoGetGVCSTran(){
+    $.ajax({
+      url: 'https://api.mongohq.com/databases/oseDB/collections/gvcsTran/documents',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        "_apikey":"xoxqi26x0rwv9loxhdfa"
+      },
+      success: function(r) {
+        console.log("Mongo: success",r[1].title);
+        createGVCSList(r);
+      },
+      error: function(data) {
+        console.log("Mongo: fail",data);
+        return false;
+      }
+     });
+  }
+
+//Returns GVCSDetails  
+  function mongoGetGVCSDetails(){
+    $.ajax({
+      url: 'https://api.mongohq.com/databases/oseDB/collections/gvcsDetails/documents',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        "_apikey":"xoxqi26x0rwv9loxhdfa"
+      },
+      success: function(r) {
+/*         console.log("Mongo: success",r); */
+        createGVCSDetail(r);
+        gvcsDetailID = r;
+      },
+      error: function(data) {
+        console.log("Mongo: fail",data);
+        return false;
+      }
+     });
+  }
+  
+//List of GVCS
+  function createGVCSList(l){
+    $('.gvcsList').empty();
+    $(l).each(function(){
+      $('.gvcsList').append('<li data-theme="c" data-transition="fade" class="gvcsData"><a href="#" data-id="'+this._id+'"><img src="'+this.imgSrc+'" alt="Thumbnail" width="100" height="100" /><h3>'+this.name+'</h3><p>'+this.status+'</p></a></li>'); 
+    });
+    $('.gvcsList').listview('refresh');
+  }
+  
+//On Click Agriculture  
+  $('#agri').on('tap', function(){
+    $('.title').html('<h1>Agriculture</h1>');
+    mongoGetGVCSList();
+    $.mobile.changePage('#gvcsDetails');
+    return false;   
+  });
+  
+//On Click Energy
+  $('#ener').on('tap', function(){
+    $('.title').html('<h1>Energy</h1>');
+    mongoGetGVCSEne();
+    $.mobile.changePage('#gvcsDetails');
+    return false;   
+  });
+  
+//On Click Industry
+  $('#indu').on('tap', function(){
+    $('.title').html('<h1>Industry</h1>');
+    mongoGetGVCSInd();
+    $.mobile.changePage('#gvcsDetails');
+    return false;   
+  });
+  
+//On Click Materials
+  $('#mate').on('tap', function(){
+    $('.title').html('<h1>Materials</h1>');
+    mongoGetGVCSMat();
+    $.mobile.changePage('#gvcsDetails');
+    return false;   
+  });
+  
+//On Click Transportation
+  $('#tran').on('tap', function(){
+    $('.title').html('<h1>Transportation</h1>');
+    mongoGetGVCSTran();
+    $.mobile.changePage('#gvcsDetails');
+    return false;   
+  });
+  
+//Detail page of GVCS
+  function createGVCSDetail(d){ 
+    $(d).each(function(){
+      $('.gvcsItemDetail').append('<img src="'+this.imgSrc+'" alt="Large Image" width="140" height="90" />');
+    });
+  }
+  
+//On Click Details  
+  $('.gvcsList').on('tap', function(){
+    mongoGetGVCSDetails();
+    console.log('ID: '+this._id);
+    console.log('gvcsDetailID: '+gvcsDetailID);
+    /*
+if(gvcsDetailID == this._id){
+      
+      $.mobile.changePage('#itemDetails');
+    }
+*/
+    return false;   
+  });
+  
+//Thank you page for Email  
+  $('#send').on('tap', function(){
+    $.mobile.changePage('#thanks');
+    $('#name').val(''); 
+    $('#email').val('');  
+    $('#mess').val('');  
+ 
+  });
+  
+//Add to MongoHQ
   function mongoCreateDocument(){
-  //return false;
-	var obj = {  "title" : "Universal Power Supply",
-	             "status" : "Pre-Prototyping",
-  	           "imgSrc" : "img/ene/universalPowerSupply.png",
-  	           "ovrView" : "Forms the backbone of an off-grid electrical system.",
-  	           "video" : "",
-  	           "uses" : "",
-  	           "works" : "" 
+	var obj = {  "imgSrc" : "img/mat/thumb/bioplasticExtruder",
+	             "name" : "Bioplastic Extractor",
+  	           "status" : "Research"
 	}
 	
 	$.ajax({
-    url: 'https://api.mongohq.com/databases/oseDB/collections/gvcsDetails/documents',
+    url: 'https://api.mongohq.com/databases/oseDB/collections/gvcsMat/documents',
     type: 'POST',
     dataType: 'json',
     data: {
@@ -51,6 +228,6 @@ $(document).ready(function() {
 
 
 
-  mongoCreateDocument();
+/*   mongoCreateDocument(); */
 });
 
